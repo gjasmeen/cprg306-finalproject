8f24ca1b1e0a3c5775d69f063088e8c127f3eab5
gjasmeen <jasmeen.garcha@edu.sait.ca>
2026-03-16 20:57:03 -0600

    temp home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,11 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-      <h1 className='text-2xl font-bold'>Fit Monkey</h1>
-      <p>Be fit like a Monkey!</p>
-      <p>Track your fitness journey with Fit Monkey!</p>
+      <div className='bg-blue-900 text-center mt-10 rounded-2xl' >
+        <h1 className='text-2xl font-bold text-white'>Fit Monkey 🐒</h1>
+        <p className='text-white'>Be fit like a Monkey!</p>
+        <p className='text-white'>Track your fitness journey with Fit Monkey!</p>
+      </div>
     </main>
   );
 }
